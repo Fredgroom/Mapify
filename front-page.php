6a4369bec96500df5edc42dbe13bca2dedda19a2
Ryan Woods <you@email.com>
2018-06-12 11:34:20 +0100

    change
    i made the very important change of messing with the classes. if you need any support on this issue please contact me

--- front-page.php
+++ front-page.php
@@ -14,10 +14,9 @@ get_header(); ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
 				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					<h1 class="page-titl screen-reader-text"><?php single_post_title(); ?></h1>
 				</header>
 
-        <?php echo ()
 			<?php endif; ?>
 
 			<?php /* Start the Loop */ ?>
